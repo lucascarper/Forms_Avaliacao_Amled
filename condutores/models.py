fb2b7fdb1ca17933103ffb4e46f009f5086c67c4
Lucas Cardoso <lucascpereira39@gmail.com>
2026-02-18 10:51:07 -0300

    v0.1.1

--- condutores/models.py
+++ condutores/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Condutor(models.Model):
-    foto = models.ImageField(upload_to='condutores/', blank=True, null=True)
+    foto = models.ImageField(
+    upload_to='condutores/',
+    null=True,
+    blank=True)
     nome = models.CharField(max_length=100)
     cargo = models.CharField(max_length=100)
     equipamento = models.CharField(max_length=100)
--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-de#hd@0m6wgbiw91tncbhuz(e6tpwy+y0itgbd&pgeua0rt=nd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.150.110']
 
 
 # Application definition
